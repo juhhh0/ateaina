9da8a314d8ed31eaae53418e9737e2499a13d3e4
Joris Mansion <juhh@juhh-core.lan>
2024-05-22 23:34:40 +0200

    escape chars

--- src/app/agency/page.tsx
+++ src/app/agency/page.tsx
@@ -8,11 +8,11 @@ export default function page() {
       <TextEffect text="Nous c'est Ateaina, formé par abel et hina, depuis 2020 on vous partage notre passions à travers des images poignantes dans nos réalisations" />
       <section className="max-width">
         <p className="display text-6xl font-bold py-4">
-          D'abord une amitié
+          D&apos;abord une amitié
         </p>
         <div className="flex items-end gap-4">
           <img className="w-1/2 aspect-video object-cover object-top" src="/ateaina.png" alt="" />
-          <p className="text-xl w-1/4">On s'est rencontré en 2019 lors de nos études Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, repellendus.</p>
+          <p className="text-xl w-1/4">On s&apos;est rencontré en 2019 lors de nos études Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, repellendus.</p>
         </div>
       </section>
       <section className="max-width mt-32">
