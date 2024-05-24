2ff6eb14e5e5c03547fc79219e295c4a0d2e83cb
Joris Mansion <juhh@juhh-core.lan>
2024-05-24 14:22:04 +0200

    youtube iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,10 @@ import { getHome } from "@/services/data/loaders";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Ateaina',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan accumsan mollis.',
-}
+  title: "Ateaina",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan accumsan mollis.",
+};
 
 export default async function Home() {
   const { data, expertises } = await getHome();
@@ -18,6 +19,17 @@ export default async function Home() {
       <TextEffect text={data.Presentation} />
       <Expertises data={expertises.data} />
       <WorksSlider />
+      <section className="max-width mx-auto flex items-center flex-col py-10">
+        <h2 className="pb-6 text-center">Retrouvez notre dernier documentaire des maintenant</h2>
+        <iframe
+        className="w-full max-w-2xl px-4 aspect-video"
+          src="https://www.youtube.com/embed/dI_hiVDvQTA?si=aD6C4uTeBvNuvXwU"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </section>
     </main>
   );
 }
